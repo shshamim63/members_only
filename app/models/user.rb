042f32e8b9df3eb081dba2228f2c5488df9b0db8
Shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-06-16 17:54:25 +0600

    final outlook and remember password works

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token, :activation_token, :reset_token
+  before_save   :downcase_email
 
   REGEX =  /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
 
@@ -11,8 +13,11 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: {minimum: 6}
 
-  before_save { email.downcase! }
-
+  def self.digest(string)
+     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                   BCrypt::Engine.cost
+     BCrypt::Password.create(string, cost: cost)
+  end
 
   def self.new_token
     SecureRandom.urlsafe_base64
@@ -23,13 +28,19 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-  def authenticated?(attribute, token)
-    digest = send("#{attribute}_digest")
-    return false if digest.nil?
-    BCrypt::Password.new(digest).is_password?(token)
-  end
-  
+  def authenticated?(remember_token)
+   return false if remember_digest.nil?
+   BCrypt::Password.new(remember_digest).is_password?(remember_token)
+ end
+
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  private
+
+  # Converts email to all lower-case.
+  def downcase_email
+    self.email = email.downcase
   end
 end
